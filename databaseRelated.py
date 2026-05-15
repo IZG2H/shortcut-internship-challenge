1e6582f51c1280b82ab252d964278baedd2593bc
IZG2H <163707034+IZG2H@users.noreply.github.com>
2026-05-15 11:47:46 +0800

    changed some comments

--- databaseRelated.py
+++ databaseRelated.py
@@ -1,13 +1,14 @@
 def databaseInsert(tableName, data): #if got time, add in creation time logs
     import sqlite3
+    from tkinter import messagebox
 
     con = sqlite3.connect('expensesTracking.db')
     cur = con.cursor()
-    #add a popup that disallow repeated names
+    #makes the table not create if there is ald another table with the same name
     cur.execute(f'''CREATE TABLE IF NOT EXISTS '{tableName}'(
-                        name TEXT, 
-                        amountNeededToPay REAL, 
-                        amountPaid REAL
+                    name TEXT, 
+                    amountNeededToPay REAL, 
+                    amountPaid REAL
                 )''') #create a table
     cur.executemany(f"INSERT INTO '{tableName}' VALUES(?, ?, ?)", data) #insert the data from the function into the database
     con.commit()
